71f28b0f460ac843ea0437d7223ba3a71f5cffc2
biradarajay1904 <biradarajay04@gmail.com>
2022-07-28 13:06:51 +0530

    modified: max integer

--- Genericsmain.java
+++ Genericsmain.java
@@ -5,6 +5,13 @@ public class Genericsmain {
 	
 	    public static void main(String[] args) {
 	     System.out.println("welcome to generics program");
+	     MaxInteger maxInt = new MaxInteger();
+	        System.out.println("Test case 1");
+	        maxInt.maxvalue(7,5,6);
+	        System.out.println("Test case 2");
+	        maxInt.maxvalue(5,6,4);
+	        System.out.println("Test case 3");
+	        maxInt.maxvalue(5,6,8);
 	    }
 	}
 
