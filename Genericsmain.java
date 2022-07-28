382e966b1ec605ff7ec1a366970b5a09abaaa966
biradarajay1904 <biradarajay04@gmail.com>
2022-07-28 13:29:14 +0530

    modified: generic method

--- Genericsmain.java
+++ Genericsmain.java
@@ -5,13 +5,25 @@ public class Genericsmain {
 	
 	    public static void main(String[] args) {
 	     System.out.println("welcome to generics program");
-	     MaxString maxString = new MaxString();
-	        System.out.println("Test case 1");
-	        maxString.maxvalue("Peach","Apple","Banana");
-	        System.out.println("Test case 2");
-	        maxString.maxvalue("Apple","Zach","Banana");
-	        System.out.println("Test case 3");
-	        maxString.maxvalue("Apple","Banana","Class");
+	     MaxValue maxValue = new MaxValue();
+	        System.out.println("Integer Test case 1");
+	        maxValue.maxvalue(5,4,3);
+	        System.out.println("Integer Test case 2");
+	        maxValue.maxvalue(5,6,3);
+	        System.out.println("Integer Test case 3");
+	        maxValue.maxvalue(5,4,7);
+	        System.out.println("Float Test case 1");
+	        maxValue.maxvalue(5.2F,4.3F,3.2F);
+	        System.out.println("Float Test case 2");
+	        maxValue.maxvalue(5.2F,6.3F,3.2F);
+	        System.out.println("Float Test case 3");
+	        maxValue.maxvalue(5.2F,4.3F,7.2F);
+	        System.out.println("String Test case 1");
+	        maxValue.maxvalue("Peach","Apple","Banana");
+	        System.out.println("String Test case 2");
+	        maxValue.maxvalue("Apple","Zach","Banana");
+	        System.out.println("String Test case 3");
+	        maxValue.maxvalue("Apple","Banana","Class");
 	    }
 	}
 
